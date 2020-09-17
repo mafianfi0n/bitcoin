26bc2548a73ed1beee745e9d7917feb959d13762
Opilane TTHK <opilane@tthk.ee>
2020-09-17 15:21:04 +0300

    New bitcoin project

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true }));
+
+app.get('/', function(request,response){
+    //console.log(request);
+    //response.send('<h1>Hello World</h1>');
+    console.log(__dirname);
+    response.sendFile(__dirname +"/index.html");
+});
+
+app.post('/',function(req,res){
+    let number1 = Number(req.body.num1);
+    let number2 = Number(req.body.num2);
+    let result = number1+number2;
+    console.log("Number 1", number1);
+    console.log("Number 2", number2);
+    console.log(req.body);
+    res.send(`${result}`);
+});
+
+app.listen(3000, function(){
+    console.log('Server is running on Port 3000');
+});
